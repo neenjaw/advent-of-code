649be68b1fe691a059e1997d9da2f95791161fe1
Tim Austin <tim@neenjaw.com>
2020-12-15 13:52:53 -0600

    only store last two times seen, use a map instead of an array for number storage

--- day15/day15_1.rb
+++ day15/day15_1.rb
@@ -1,17 +1,19 @@
 class MemoryNumber
-  attr_reader :number, :turns_seen
+  attr_reader :number, :last_turn, :before_last_turn
 
   def initialize(number, turn_seen)
     @number = number
-    @turns_seen = [turn_seen]
+    @last_turn = turn_seen
+    @before_last_turn = nil
   end
 
   def saw_again(turn)
-    turns_seen << turn
+    @before_last_turn = last_turn
+    @last_turn = turn
   end
 
   def times_seen
-    turns_seen.count
+    [last_turn, before_last_turn].compact.count
   end
 end
 
@@ -20,10 +22,10 @@ starting_turn = starting_numbers.count + 1
 ending_turn = 30_000_000
 
 number_memory = {}
-turn_memory = [:start]
+turn_memory = {}
 starting_numbers.each.with_index(1) do |number, turn|
   number_memory[number] = MemoryNumber.new(number, turn)
-  turn_memory << number
+  turn_memory[turn] = number
 end
 
 turn = starting_turn
@@ -34,8 +36,9 @@ loop do
   if number_memory[last_number].times_seen == 1
     current_number = 0
   else
-    turn_seen_before_last, last_turn  = number_memory[last_number].turns_seen.last(2)
-    current_number = last_turn - turn_seen_before_last
+    turn_seen_before_last = number_memory[last_number].before_last_turn
+    last_turn_seen = number_memory[last_number].last_turn
+    current_number = last_turn_seen - turn_seen_before_last
   end
 
   if number_memory[current_number]
@@ -44,12 +47,19 @@ loop do
     number_memory[current_number] = MemoryNumber.new(current_number, turn)
   end
 
-  turn_memory << current_number
+  turn_memory[turn] = current_number
 
   break if turn == ending_turn
+
+  if (turn % 1_000_000 == 0)
+    puts turn
+  end
 
   turn += 1
 end
 
+puts turn_memory[turn]
+
 # puts starting_numbers
-puts turn_memory[ending_turn]
+# puts turn_memory[2020]
+# puts turn_memory[ending_turn]
