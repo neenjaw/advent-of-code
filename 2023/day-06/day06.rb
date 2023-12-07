7ec44c981705b428257f2d410f803d2af05dddbb
Tim Austin <tim@neenjaw.com>
2023-12-06 22:57:47 -0600

    cleaned up by copilot 🤩

--- 2023/day-06/day06.rb
+++ 2023/day-06/day06.rb
@@ -1,95 +1,98 @@
-# Time:      7  15   30
-# Distance:  9  40  200
-
-input = ARGF.read.chomp.split("\n")
-  .map do |line|
-    line.chomp.split.drop(1).map(&:to_i)
+class RaceCalculator
+  def initialize(input)
+    @input = input
   end
 
-races =
-  input
-  .transpose
-  .map do |(time, distance_record)|
-    wins = 0
-    pause = 0
+  def calculate_races
+    races = @input.transpose.map do |time, distance_record|
+      wins = 0
+      pause = 0
 
-    while pause < time
-      pause += 1
-      distance = pause * (time - pause)
-      wins += 1 if distance > distance_record
+      while pause < time
+        pause += 1
+        distance = pause * (time - pause)
+        wins += 1 if distance > distance_record
+      end
+
+      wins
     end
 
-    wins
+    races.inject(&:*)
   end
-  .inject(&:*)
 
-pp races
+  def calculate_win_start
+    time, distance_record = @input.map(&:join).map(&:to_i)
 
-time, distance_record = input.map(&:join)
-  .map(&:to_i)
+    min = 0
+    max = time
+    middle = (min + max) / 2
+    win_start = -1
 
+    while min < max
+      pause = middle
+      prev_pause = pause - 1
 
-min = 0
-max = time
-middle = (min + max) / 2
-iterations = 0
-win_start = -1
+      distance = pause * (time - pause)
+      prev_distance = prev_pause * (time - prev_pause)
 
-while min < max
-  iterations += 1
-  pause = middle
-  prev_pause = pause - 1
+      if distance > distance_record && prev_distance <= distance_record
+        win_start = pause
+        break
+      elsif prev_distance > distance_record
+        max = middle - 1
+      elsif distance <= distance_record
+        min = middle + 1
+      end
+      middle = (min + max) / 2
+    end
 
-  distance = pause * (time - pause)
-  prev_distance = prev_pause * (time - prev_pause)
-
-  if distance > distance_record && prev_distance <= distance_record
-    win_start = pause
-    break
-  elsif prev_distance > distance_record
-    max = middle - 1
-  elsif distance <= distance_record
-    min = middle + 1
+    win_start
   end
-  middle = (min + max) / 2
+
+  def calculate_win_end
+    time, distance_record = @input.map(&:join).map(&:to_i)
+
+    min = 0
+    max = time
+    middle = (min + max) / 2
+    win_end = -1
+
+    while min < max
+      pause = middle
+      next_pause = pause + 1
+
+      distance = pause * (time - pause)
+      next_distance = next_pause * (time - next_pause)
+
+      if distance > distance_record && next_distance <= distance_record
+        win_end = pause
+        break
+      elsif next_distance > distance_record
+        min = middle + 1
+      elsif distance <= distance_record
+        max = middle - 1
+      end
+      middle = (min + max) / 2
+    end
+
+    win_end = max if win_end == -1
+
+    win_end
+  end
+
+  def calculate_win_length
+    win_end = calculate_win_end
+    win_start = calculate_win_start
+
+    win_end - win_start + 1
+  end
 end
 
-pp win_start
+input = ARGF.read.chomp.split("\n").map { |line| line.chomp.split.drop(1).map(&:to_i) }
 
-min = 0
-max = time
-middle = (min + max) / 2
-iterations = 0
-win_end = -1
+race_calculator = RaceCalculator.new(input)
 
-while min < max
-  iterations += 1
-  pause = middle
-  next_pause = pause + 1
-
-  puts "min: #{min}\nmax: #{max}\nmiddle: #{middle}\npause: #{pause}\nnext_pause: #{next_pause}"
-
-
-  distance = pause * (time - pause)
-  next_distance = next_pause * (time - next_pause)
-
-  puts "distance: #{distance}\nnext_distance: #{next_distance}\n\n"
-
-  if distance > distance_record && next_distance <= distance_record
-    win_end = pause
-    break
-  elsif next_distance > distance_record
-    min = middle + 1
-  elsif distance <= distance_record
-    max = middle - 1
-  end
-  middle = (min + max) / 2
-end
-
-if win_end == -1
-  win_end = max
-end
-
-pp win_end
-
-pp win_end - win_start + 1
+pp race_calculator.calculate_races
+pp race_calculator.calculate_win_start
+pp race_calculator.calculate_win_end
+pp race_calculator.calculate_win_length
