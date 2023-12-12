1c515bd435d8bf188c11febcba9e525979dd9a62
Tim Austin <tim@neenjaw.com>
2023-12-12 08:05:10 -0600

    Day 12
    In the realm of algorithms, where puzzles reign supreme,
    Dynamic Programming's craft, a maze of intricate scheme.
    A tale of computation, where hope and despair collide,
    Broken springs and broken dreams in algorithms do abide.
    
    Like stars in cosmic dance, these problems unfold,
    Each subproblem a universe waiting to be told.
    But oh, the woes of recursion, the endless loops in time,
    As broken springs echo broken dreams in rhythm and in rhyme.
    
    The promise of optimization, efficiency's gleaming light,
    Shattered against complexity, a formidable sight.
    For as the universe expands, so do these problems grow,
    Broken springs and shattered dreams, a symphony of woe.
    
    In the labyrinth of memoization, memories may decay,
    Akin to the heat death of the cosmos, they slowly fade away.
    What once seemed so promising, a solution within grasp,
    Now entwined with broken springs, an eternal, fruitless clasp.
    
    Dynamic Programming, a paradoxical art,
    A blend of hope and sorrow that tears the soul apart.
    As the universe marches towards its cosmic end,
    Broken springs and shattered dreams persist, refusing to bend.
    
    Yet amidst these trials, a spark of hope remains,
    For within the chaos, resilience often gains.
    Dynamic Programming's woes, a testament to strife,
    But through persistence, broken dreams may find new life.

--- 2023/day-12/day12.rb
+++ 2023/day-12/day12.rb
@@ -1,73 +1,84 @@
 require 'set'
 
-# #.#.### 1,1,3
-# .#...#....###. 1,1,3
-# .#.###.#.###### 1,3,1,6
-# ####.#...#... 4,1,1
-# #....######..#####. 1,6,5
-# .###.##....# 3,2,1
+def dfs(memo, rule, clues, rule_index, clue_index, weight)
+  # Do we have the solution already?
+  return memo[[rule_index, clue_index, weight]] if memo.key?([rule_index, clue_index, weight])
 
-# ???.### 1,1,3
-# .??..??...?##. 1,1,3
-# ?#?#?#?#?#?#?#? 1,3,1,6
-# ????.#...#... 4,1,1
-# ????.######..#####. 1,6,5
-# ?###???????? 3,2,1
+  # Are we at the base case?
+  if rule_index == rule.length
+    all_clues_used = clue_index == clues.length && weight.zero?
+    last_char_completes_clue = clue_index == clues.length - 1 && weight == clues[clue_index]
 
+    if (all_clues_used || last_char_completes_clue)
+      return 1
+    else
+      return 0
+    end
+  end
+
+  step_total = 0
+  char = rule[rule_index]
+
+  # need to get to our base case to progressively build up our answer
+  # so as we walk through the rule, we need to consider the case where
+  # 1. the current rule character is a dot or unknown
+  # 2. the current rule character is a hash or unknown
+  # validating the state against that choice, and then recursing to the next
+  # character in the rule to then consider the validity of the next choices
+
+  # consider the case where the current rule character is a dot or unknown
+  is_dot_or_unknown = char == '.' || char == '?'
+
+  if is_dot_or_unknown
+    if weight.zero?
+      step_total += dfs(memo, rule, clues, rule_index + 1, clue_index, 0)
+    elsif clues[clue_index] == weight
+      step_total += dfs(memo, rule, clues, rule_index + 1, clue_index + 1, 0)
+    end
+  end
+
+  # consider the case where the current rule character is a hash or unknown
+  is_hash_or_unknown = char == '#' || char == '?'
+  is_before_the_last_clue = clue_index < clues.length
+  is_clue_still_possible = is_before_the_last_clue && weight < (clues[clue_index])
+
+  if is_hash_or_unknown && is_clue_still_possible
+    step_total += dfs(memo, rule, clues, rule_index + 1, clue_index, weight + 1)
+  end
+
+  # Remember the solution for this subproblem
+  memo[[rule_index, clue_index, weight]] = step_total
+
+  step_total
+end
 
 def read
   lines = []
   ARGF.each_line do |line|
-    pattern, ns = line.chomp.split
-    counts = ns.split(',').map(&:to_i)
-
-    lines << [pattern.chars, counts]
+    rule, clues = line.split
+    clues = clues.split(",").map(&:to_i)
+    lines << [rule, clues]
   end
   lines
 end
 
-def compute_possibilities(row)
-  possibilities = []
-  unknown_cells = row[0].count('?') # Count the number of empty cells in the row
-  options = ['#', '.']
-  all_permutations = options.repeated_permutation(unknown_cells).to_a
+def part1(rules)
+  rules.inject(0) do |total, (rule, clues)|
+    total += dfs({}, rule, clues, 0, 0, 0)
 
-  [all_permutations, *row ]
-end
-
-def substitute_possibilities(possibilities, pattern)
-  possibilities.map do |possibility|
-    pattern.map { |c| c == '?' ? possibility.shift : c}
+    total
   end
 end
 
-def get_subgroup_size(group)
-  split_group = group.join.sub(/^\.+/, '').sub(/\.+$/, '').split(/\.+/)
-  split_group.map { |sg| sg.count('#') }
-end
+def part2(rules)
+  rules.inject(0) do |total, (rule, clues)|
+    rule, clues = ("#{rule}?" * 5).delete_suffix('?').chars, clues * 5
+    total += dfs({}, rule, clues, 0, 0, 0)
 
-def each_group(array)
-  i = 0
-  array.chunk { |c| c }.each do |char, group|
-    yield [group, i] if char == '#'
-    i += 1
+    total
   end
 end
 
-lines = read
-
-total = lines.map.with_index do |row, i|
-  possibilities, pattern, counts = compute_possibilities(row)
-  substituted_possibilities = substitute_possibilities(possibilities, pattern)
-
-  valid_count = 0
-  i = 0
-
-  substituted_possibilities.inject(0) do |valid_count, possibility|
-    chunks = possibility.chunk { |c| c }.select { |char, group| char == '#' }.map { |char, group| group.length }
-    valid_count += 1 if chunks == counts
-    valid_count
-  end
-end.sum
-
-pp total
+rules = read
+puts part1(rules)
+puts part2(rules)
